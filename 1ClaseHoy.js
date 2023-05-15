c99cd101f389e52a1c997f037503312cee9850ce
andressantage <ingeniero.andres.santana@gmail.com>
2023-05-15 17:52:20 -0500

    cambio

---
+++ 1ClaseHoy.js
@@ -0,0 +1,31 @@
+URI: Uniform Resource Identifier de una API, url especifica de la api
+Ejemplo con Json-server: http://localhost:4001/
+
+Endpoint: Interface de Programación de Aplicacionesm url a donde se hace las peticiones de la api
+Ejemplo con Json-server: http://localhost:4001/clientes  ...en clientes esta la informacion especifica y asi para las demas rutas
+Ejemplo con Json-server: http://localhost:4001/rutas
+Ejemplo con Json-server: http://localhost:4001/personal
+
+
+Comienzo de hacer solicitudes a una API:
+
+La pagina https://rapidapi.com tiene organizada la forma de peticiones para una API
+
+Ir a la URL: https://rapidapi.com/Glavier/api/youtube138/
+Ir a pricing: y darle en suscribir en la parte gratis obvio
+Ir a Endpoints: a la izquierda ir a "Video", luego a "Video Details"
+Ir a la mitad de la pantalla e ir a "Required Parameters" y por ejemplo:
+    si el link de un video es: https://www.youtube.com/watch?v=L7X0c36mgqY
+    copiar solo esta parte L7X0c36mgqY
+    ahi en id modificar y pegar: L7X0c36mgqY
+Luego ir a "Optional Parameters" y en "hl" poner "en", que es en ingles
+Ir a la derecha y donde esta "node" cambiarlo a "Javascript/fetch"
+Ir a la mitad y dar en "Test Endpoint", luego a la derecha mostrara los resultados de esa peticion
+En este caso en los resultados habra informacion principalmente de los detllaes del video como: me gustas, publicacion del video y otros
+
+
+
+
+para buscar a alguien que sepa en discord:
+ir a #presentacion y luego en buscar y ahi lo que se necesita buscar, en el input de buscar: #presentacion DescripcionBusqueda
+
